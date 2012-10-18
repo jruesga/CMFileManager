44100f9a6bbd5fa9eca96c6970115b5ac53c7212
jruesga <jorge@ruesga.com>
2012-10-19 01:03:41 +0200

    Add a method to check if the current console is privileged

--- src/com/cyanogenmod/explorer/console/ConsoleBuilder.java
+++ src/com/cyanogenmod/explorer/console/ConsoleBuilder.java
@@ -395,4 +395,16 @@ public final class ConsoleBuilder {
         }
     }
 
+    /**
+     * Method that returns if the current console is a privileged console
+     *
+     * @return boolean If the current console is a privileged console
+     */
+    public static boolean isPrivileged() {
+        if (sHolder != null && sHolder.getConsole() instanceof PrivilegedConsole) {
+            return true;
+        }
+        return false;
+    }
+
 }

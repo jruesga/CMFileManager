d921b98dadb5ed0e165804c5bf964df708b5fba0
jruesga <jorge@ruesga.com>
2012-10-28 19:18:03 +0100

    New methods for deal with Android

--- src/com/cyanogenmod/filemanager/util/AndroidHelper.java
+++ src/com/cyanogenmod/filemanager/util/AndroidHelper.java
@@ -18,6 +18,8 @@ package com.cyanogenmod.filemanager.util;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
 import android.view.ViewConfiguration;
 
 /**
@@ -46,6 +48,33 @@ public final class AndroidHelper {
     public static boolean showOptionsMenu(Context ctx) {
         // Show overflow button?
         return !ViewConfiguration.get(ctx).hasPermanentMenuKey();
+    }
+
+    /**
+     * This method converts dp unit to equivalent device specific value in pixels. 
+     * 
+     * @param ctx The current context
+     * @param dp A value in dp (Device independent pixels) unit
+     * @return float A float value to represent Pixels equivalent to dp according to device
+     */
+    public static float convertDpToPixel(Context ctx, float dp){
+        Resources resources = ctx.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        return dp * (metrics.densityDpi / 160f);
+    }
+
+    /**
+     * This method converts device specific pixels to device independent pixels.
+     * 
+     * @param ctx The current context
+     * @param px A value in px (pixels) unit
+     * @return A float value to represent dp equivalent to px value
+     */
+    public static float convertPixelsToDp(Context ctx, float px) {
+        Resources resources = ctx.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        return px / (metrics.densityDpi / 160f);
+
     }
 
 }

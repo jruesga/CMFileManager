13c394d209b2ee6fbea1e1ecf47046d96a52e71c
Jorge Ruesga <jorge@ruesga.com>
2012-11-20 00:17:36 +0100

    CMFileManager: Check shell commands on startup
    This change improved the detection of CM rooted devices with support for all the
    commands used by CMFM. In case of one of this command was not detected, the app start
    in chrooted-java mode (with limited support).
    
    Change-Id: Ie0f61d74f9619f476ea517c1b3c03ec453e033f3

--- src/com/cyanogenmod/filemanager/FileManagerApplication.java
+++ src/com/cyanogenmod/filemanager/FileManagerApplication.java
@@ -203,9 +203,7 @@ public final class FileManagerApplication extends Application {
         sIsDebuggable = (0 != (getApplicationInfo().flags &= ApplicationInfo.FLAG_DEBUGGABLE));
 
         // Check if the device is rooted
-        sIsDeviceRooted =
-                new File(getString(R.string.su_binary)).exists() &&
-                getSystemProperty("ro.cm.version") != null; //$NON-NLS-1$
+        sIsDeviceRooted = areShellCommandsPresent();
 
         // Register the notify broadcast receiver
         IntentFilter filter = new IntentFilter();
@@ -419,4 +417,41 @@ public final class FileManagerApplication extends Application {
         }
     }
 
+    /**
+     * Method that check if all shell commands are present in the device
+     *
+     * @return boolean Check if the device has all of the shell commands
+     */
+    private boolean areShellCommandsPresent() {
+        try {
+            String shellCommands = getString(R.string.shell_required_commands);
+            String[] commands = shellCommands.split(","); //$NON-NLS-1$
+            int cc = commands.length;
+            if (cc == 0) {
+                //???
+                Log.w(TAG, "No shell commands."); //$NON-NLS-1$
+                return false;
+            }
+            for (int i = 0; i < cc; i++) {
+                String c = commands[i].trim();
+                if (c.length() == 0) continue;
+                File cmd = new File(c);
+                if (!cmd.exists() || !cmd.isFile()) {
+                    Log.w(TAG,
+                            String.format(
+                                    "Command %s not found. Exists: %s; IsFile: %s.", //$NON-NLS-1$
+                                    c,
+                                    String.valueOf(cmd.exists()),
+                                    String.valueOf(cmd.isFile())));
+                    return false;
+                }
+            }
+            // All commands are present
+            return true;
+        } catch (Exception e) {
+            Log.e(TAG,
+                    "Failed to read shell commands.", e); //$NON-NLS-1$
+        }
+        return false;
+    }
 }

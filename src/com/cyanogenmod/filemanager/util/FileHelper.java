LineageOS/android_packages_apps_CMFileManager

Issue 6743: CM FileManager can't recognize file types if named with capital suffix
Issue: http://code.google.com/p/cyanogenmod/issues/detail?id=6743

Added case compare to extension and mime/types.

Change-Id: I394472e03b5a92590088fdebbb75dd13ee4bade2

src/com/cyanogenmod/filemanager/util/FileHelper.java
@@ -825,9 +825,10 @@ public final class FileHelper {
         }
         String ext = getExtension(fso);
         if (ext != null) {
+            String extLC = ext.toLowerCase();
             int cc = VALID.length;
             for (int i = 0; i < cc; i++) {
-                if (VALID[i].compareTo(ext) == 0) {
+                if (VALID[i].compareTo(extLC) == 0) {
                     return true;
                 }
             }

jruesga/CMFileManager

src/com/cyanogenmod/filemanager/util/FileHelper.java
@@ -827,7 +827,7 @@ public final class FileHelper {
         if (ext != null) {
             int cc = VALID.length;
             for (int i = 0; i < cc; i++) {
-                if (VALID[i].compareTo(ext) == 0) {
+                if (VALID[i].compareToIgnoreCase(ext) == 0) {
                     return true;
                 }
             }
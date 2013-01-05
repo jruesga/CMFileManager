7be3bee3674897d2618a20c1f72b77d9d1f9261f
jruesga <jorge@ruesga.com>
2013-01-05 16:48:15 +0100

    New getAbsolutePath method
    Signed-off-by: jruesga <jorge@ruesga.com>

--- src/com/cyanogenmod/filemanager/util/CommandHelper.java
+++ src/com/cyanogenmod/filemanager/util/CommandHelper.java
@@ -420,41 +420,6 @@ public final class CommandHelper {
     }
 
     /**
-     * Method that retrieves the absolute path of a file or directory.
-     *
-     * @param context The current context (needed if console == null)
-     * @param path The short path
-     * @param console The console in which execute the program. <code>null</code>
-     * to attach to the default console
-     * @return String The absolute path of the directory
-     * @throws FileNotFoundException If the initial directory not exists
-     * @throws IOException If initial directory couldn't be checked
-     * @throws InvalidCommandDefinitionException If the command has an invalid definition
-     * @throws NoSuchFileOrDirectory If the file or directory was not found
-     * @throws ConsoleAllocException If the console can't be allocated
-     * @throws InsufficientPermissionsException If an operation requires elevated permissions
-     * @throws CommandNotFoundException If the command was not found
-     * @throws OperationTimeoutException If the operation exceeded the maximum time of wait
-     * @throws ExecutionException If the operation returns a invalid exit code
-     * @see ResolveLinkExecutable
-     */
-    public static String getAbsolutePath(Context context, String path, Console console)
-            throws FileNotFoundException, IOException, ConsoleAllocException,
-            NoSuchFileOrDirectory, InsufficientPermissionsException,
-            CommandNotFoundException, OperationTimeoutException,
-            ExecutionException, InvalidCommandDefinitionException {
-        Console c = ensureConsole(context, console);
-        ResolveLinkExecutable executable =
-                c.getExecutableFactory().newCreator().createResolveLinkExecutable(path);
-        execute(context, executable, c);
-        FileSystemObject fso = executable.getResult();
-        if (fso == null) {
-            return null;
-        }
-        return fso.getFullPath();
-    }
-
-    /**
      * Method that resolves a symlink to its real file system object.
      *
      * @param context The current context (needed if console == null)

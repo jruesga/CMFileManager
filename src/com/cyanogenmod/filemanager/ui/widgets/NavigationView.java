f2a9335b58e7922d79ff6744cc65ab98a4f551de
Jorge Ruesga <jorge@ruesga.com>
2013-01-07 22:45:27 +0100

    CMFM: Fix FC caused by accessing ui outside of UI thread
    Patchset 2: Formatting
    
    Change-Id: I1323b4f92a64dd23afe4aa28d3569ce01d72c039
    Signed-off-by: jruesga <jorge@ruesga.com>

--- src/com/cyanogenmod/filemanager/ui/widgets/NavigationView.java
+++ src/com/cyanogenmod/filemanager/ui/widgets/NavigationView.java
@@ -906,14 +906,20 @@ public class NavigationView extends RelativeLayout implements
                          *
                          * @param out Fade out (true); Fade in (false)
                          */
-                        void fadeEfect(boolean out) {
-                            Animation fadeAnim = out ?
-                                                     new AlphaAnimation(1, 0) :
-                                                     new AlphaAnimation(0, 1);
-                            fadeAnim.setDuration(50L);
-                            fadeAnim.setFillAfter(true);
-                            fadeAnim.setInterpolator(new AccelerateInterpolator());
-                            NavigationView.this.startAnimation(fadeAnim);
+                        void fadeEfect(final boolean out) {
+                            Activity activity = (Activity)getContext();
+                            activity.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Animation fadeAnim = out ?
+                                            new AlphaAnimation(1, 0) :
+                                            new AlphaAnimation(0, 1);
+                                    fadeAnim.setDuration(50L);
+                                    fadeAnim.setFillAfter(true);
+                                    fadeAnim.setInterpolator(new AccelerateInterpolator());
+                                    NavigationView.this.startAnimation(fadeAnim);
+                                }
+                            });
                         }
                    };
             task.execute(fNewDir);

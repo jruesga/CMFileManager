889a29e8bd4b0fd6102cbd91e49ad353383df930
Martin Brabham <optedoblivion@cyngn.com>
2015-04-17 12:36:21 -0700

    Make the new filename max length error message show.
    Change-Id: Ic997110fb78d253c7d3602a5b81157fceb9f73b2

--- src/com/cyanogenmod/filemanager/ui/dialogs/InputNameDialog.java
+++ src/com/cyanogenmod/filemanager/ui/dialogs/InputNameDialog.java
@@ -262,7 +262,7 @@ public class InputNameDialog
         }
 
         // Too long
-        if (name.length() > FILENAME_CHAR_LIMIT) {
+        if (name.length() >= FILENAME_CHAR_LIMIT) {
             setMsg(InputNameDialog.this.mContext.getString(
                     R.string.input_name_dialog_message_invalid_name_length), true);
             return;

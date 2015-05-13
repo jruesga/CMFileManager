7cf35abef8a342effac7dff4b2b401a9d4514e89
Stephen Bird <sebirdman@gmail.com>
2015-05-12 17:30:16 -0700

    CM Easy Mode File Manager Navigation fixes
    This patch fixes an issue where the user cannot
    get back to easy mode without exiting and re-entering
    the application in certain situations
    
    Repro Steps:
    1) Select "All" in the easy mode list
    2) Action Bar arrow is gone, pressing back on the navbar
    does not return you to easy mode
    
    Change-Id: I5f6b47b8b69e57cffac77f7bfa24f8a75d8ff1d4
    (cherry picked from commit c5c685221337c3c0b1f13ef20423de56fdba8870)

--- src/com/cyanogenmod/filemanager/activities/NavigationActivity.java
+++ src/com/cyanogenmod/filemanager/activities/NavigationActivity.java
@@ -471,6 +471,8 @@ public class NavigationActivity extends Activity
 
     private int mOrientation;
 
+    private boolean mNeedsEasyMode = false;
+
     /**
      * @hide
      */
@@ -681,6 +683,16 @@ public class NavigationActivity extends Activity
     public boolean onOptionsItemSelected(MenuItem item) {
         if (mDrawerToggle.onOptionsItemSelected(item)) {
           return true;
+        }
+
+        if (mNeedsEasyMode) {
+            if (item.getItemId() == android.R.id.home) {
+                if (mHistory.size() == 0 && !isEasyModeVisible()) {
+                    performShowEasyMode();
+                } else {
+                    back();
+                }
+            }
         }
         return super.onOptionsItemSelected(item);
     }
@@ -967,6 +979,7 @@ public class NavigationActivity extends Activity
     private void performShowEasyMode() {
         mEasyModeListView.setVisibility(View.VISIBLE);
         getCurrentNavigationView().setVisibility(View.GONE);
+        performShowBackArrow(false);
     }
 
     /**
@@ -975,6 +988,16 @@ public class NavigationActivity extends Activity
     private void performHideEasyMode() {
         mEasyModeListView.setVisibility(View.GONE);
         getCurrentNavigationView().setVisibility(View.VISIBLE);
+    }
+
+    private void performShowBackArrow(boolean showBackArrow) {
+        if (mNeedsEasyMode) {
+            mDrawerToggle.setDrawerIndicatorEnabled(!showBackArrow);
+        }
+    }
+
+    private boolean isEasyModeVisible() {
+        return mEasyModeListView.getVisibility() != View.GONE;
     }
 
     /**
@@ -1083,6 +1106,7 @@ public class NavigationActivity extends Activity
                         } else {
                             performHideEasyMode();
                         }
+                        performShowBackArrow(!mDrawerToggle.isDrawerIndicatorEnabled());
                         getCurrentNavigationView().open(fso);
                         mDrawerLayout.closeDrawer(Gravity.START);
                     }
@@ -1458,7 +1482,9 @@ public class NavigationActivity extends Activity
         intent.putExtra(SearchManager.QUERY, "*"); // Use wild-card '*'
 
         if (position == 0) {
+            // the user has selected all items, they want to see their folders so let's do that.
             performHideEasyMode();
+            performShowBackArrow(true);
             return;
 
         } else {
@@ -1656,8 +1682,9 @@ public class NavigationActivity extends Activity
             }
         }
 
-        needsEasyMode = needsEasyMode
-                && getResources().getBoolean(R.bool.cmcc_show_easy_mode);
+        mNeedsEasyMode = getResources().getBoolean(R.bool.cmcc_show_easy_mode);
+
+        needsEasyMode = needsEasyMode && mNeedsEasyMode;
         if (needsEasyMode) {
             performShowEasyMode();
         } else {
@@ -1747,6 +1774,11 @@ public class NavigationActivity extends Activity
         if (checkBackAction()) {
             performHideEasyMode();
             return;
+        } else {
+            if (mNeedsEasyMode && !isEasyModeVisible()) {
+                performShowEasyMode();
+                return;
+            }
         }
 
         // An exit event has occurred, force the destroy the consoles
@@ -2133,7 +2165,11 @@ public class NavigationActivity extends Activity
             //Communicate the user that the next time the application will be closed
             this.mExitBackTimeout = System.currentTimeMillis();
             DialogHelper.showToast(this, R.string.msgs_push_again_to_exit, Toast.LENGTH_SHORT);
-            return true;
+            if (mNeedsEasyMode) {
+                return isEasyModeVisible();
+            } else {
+                return true;
+            }
         }
 
         //Back action not applied
